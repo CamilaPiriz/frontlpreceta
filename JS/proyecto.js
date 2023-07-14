9f1500081d4f92e33e3d43560832e7c59389c56b
CamilaPiriz <132920378+CamilaPiriz@users.noreply.github.com>
2023-07-14 11:16:08 -0300

    Add files via upload

---
+++ JS/proyecto.js
@@ -0,0 +1,8 @@
+alert("Aqui encontraran las mejores recetas regiolanes, BIENVENIDOS")
+
+function Ayudavisual() {
+  document.body.style.backgroundColor="black";
+    document.nav.style.backgroundColor="black"
+      document.footer.style.backgroundColor="black"
+
+}
